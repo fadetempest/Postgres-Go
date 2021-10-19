41638e724bcbfcf331a7487af98965ad568ddda6
fadetempest <73659348+fadetempest@users.noreply.github.com>
2021-10-19 18:52:17 +0400

    Add files via upload

---
+++ pkg/server/server.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	Ctx context.Context
+	Address string
+}
+
+func NewServer(ctx context.Context, address string) *Server{
+	return &Server{
+		Ctx: ctx,
+		Address: address,
+	}
+}
+
+func (s *Server) Run() error{
+	r:= mux.NewRouter()
+	r.HandleFunc("/", menu)
+	srv:=&http.Server{
+		Addr: s.Address,
+		Handler: r,
+	}
+	log.Printf("Server is running on %s", s.Address)
+	return srv.ListenAndServe()
+}
+
+func menu(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello world"))
+}
