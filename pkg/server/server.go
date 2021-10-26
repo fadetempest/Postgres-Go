78d56f66a24010212468f2a1e794c6c08a6e32e4
fadetempest <73659348+fadetempest@users.noreply.github.com>
2021-10-26 17:42:39 +0400

    Update server.go

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,39 +2,69 @@ package server
 
 import (
 	"context"
+	"database/sql"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"restaurant/base"
 	"restaurant/tools"
 )
 
 type Server struct {
 	ctx context.Context
 	Address string
+	DatabaseUrl string
 }
 
-type Methods struct {
-
+type Handl struct {
+	r *mux.Router
+	Base *base.DishRepo
 }
 
-func NewServer(ctx context.Context, address string) *Server{
+func NewServer(ctx context.Context, address string, database string) *Server{
 	return &Server{
 		ctx: ctx,
 		Address: address,
+		DatabaseUrl: database,
 	}
 }
 
 func (s *Server) Run() error{
-	r:= mux.NewRouter()
-	r.HandleFunc("/menu", tools.Menu)
-	r.HandleFunc("/add", tools.AddMeal)
-	r.HandleFunc("/delete/{id}", tools.DelMeal)
-	r.HandleFunc("/update", tools.UpdateMeal)
+	db, err:= openDb(s.DatabaseUrl)
+	if err != nil{
+		log.Println("Error while opening DataBase")
+	}
+	defer db.Close()
+
+	rp:= &Handl{
+		r: mux.NewRouter(),
+		Base: base.NewDishRepo(db),
+	}
+
+	store:=tools.NewMethods(rp.Base)
+
+	rp.r.HandleFunc("/add", store.AddMeal)
+	rp.r.HandleFunc("/menu", store.Menu)
+	rp.r.HandleFunc("/delete/{id}", store.DelMeal)
+	rp.r.HandleFunc("/update", store.UpdateMeal)
+
 	srv:=&http.Server{
 		Addr: s.Address,
-		Handler: r,
+		Handler: rp.r,
 	}
+
 	log.Printf("Server is running on %s", s.Address)
 	return srv.ListenAndServe()
+}
+
+func openDb(baseUrl string) (*sql.DB, error){
+	db, err:=sql.Open("postgres", baseUrl)
+	if err!=nil{
+		return nil, err
+	}
+	if err:=db.Ping();err!=nil{
+		return nil,err
+	}
+	return db,nil
 }
 
